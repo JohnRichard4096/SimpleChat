fce71b414db2f9509aa1d0747c69f6d2a5f46e4e
John Richard <67693593+JohnRichard4096@users.noreply.github.com>
2024-04-26 18:08:25 +0800

    初次提交

---
+++ src/main/templates/com/simplechat/velocity/schat/BuildConstants.java
@@ -0,0 +1,7 @@
+package com.simplechat.velocity.schat;
+
+// The constants are replaced before compilation
+public class BuildConstants {
+
+    public static final String VERSION = "${version}";
+}
